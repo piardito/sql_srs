d65ea64dfbbb583dc75afe1da776d2ecda79bdf5
piardito <piardito>
2025-09-25 16:42:09 +0200

    modification du code avec try_except

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pandas as pd
 import duckdb as db
 import io
 
+st.write("""# SQL SRS
+    Spaced Repetition System SQL practice""")
+
 csv = '''
 beverage,price
 orange juice,2.5
@@ -19,19 +22,44 @@ chocolatine,2
 muffin,3
 """
 
-food_items = pd.read_csv(io.StringIO(csv))
+food_items = pd.read_csv(io.StringIO(csv2))
 
-answer = """
+answer_str = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
-solution = db.query(answer)
+solution_df = db.query(answer_str).df()
+
+with st.sidebar :
+    option = st.selectbox(
+        "What would you like to review",
+        ['Join','Groupby','Windows Functions'],
+        index = None,
+        placeholder = "Select a theme ..."
+    )
+    st.write(f"You selected : {option}")
 
 st.header("enter your code")
 query = st.text_area(label = "votre code SQL ici",key = "user_input")
 if query:
-    result = db.query(query)
+    result = db.query(query).df()
     st.dataframe(result)
+
+    if len(result.columns) != len(solution_df.columns):
+        st.write("Some colums are missing")
+    try :
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("some columns are missing")
+
+
+    n_lines_differences = result.shape[0] - solution_df.shape[0]
+    if n_lines_differences !=0 :
+        st.write(
+            f"result has a {n_lines_differences} lines differences with the solution"
+        )
+
 
 
 tab2,tab3 = st.tabs(['Tables','Solution'])
@@ -42,9 +70,9 @@ with tab2:
     st.write("table : food_items")
     st.dataframe(food_items)
     st.write("expected : ")
-    st.dataframe(solution)
+    st.dataframe(solution_df)
 
 with tab3 :
-    st.write(answer)
+    st.write(answer_str)
 
 
