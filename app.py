eecfacb3844e799a37c1999a56f1fb2e1f5cf0ca
piardito <piardito>
2025-09-25 15:41:20 +0200

    changement

--- app.py
+++ app.py
@@ -45,22 +45,6 @@ if query:
     result = db.query(query)
     st.dataframe(result)
 
-
-with st.sidebar :
-    option = st.selectbox(
-        "What would you like to review",
-        ['Join','Groupby','Windows Functions'],
-        index = None,
-        placeholder = "Select a theme ..."
-    )
-
-    st.write(f"You selected : {option}")
-
-data = {"a": [1,2,3],"b":[4,5,6]}
-df = pd.DataFrame(data)
-
-
-
 tab2,tab3 = st.tabs(['Tables','Solution'])
 
 with tab2:
