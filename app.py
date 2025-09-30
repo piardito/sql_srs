f8e4439e56f597e85e45a35ead5f9adb91c683bd
piardito <piardito>
2025-09-30 10:57:42 +0200

    changement

--- app.py
+++ app.py
@@ -1,51 +1,55 @@
-import streamlit as st
-import pandas as pd
-import duckdb as db
+# pylint: disable=missing-module-docstring
 import io
 
-st.write("""# SQL SRS
-    Spaced Repetition System SQL practice""")
+import duckdb as db
+import pandas as pd
+import streamlit as st
 
-csv = '''
+st.write(
+    """# SQL SRS
+    Spaced Repetition System SQL practice"""
+)
+
+CSV = """
 beverage,price
 orange juice,2.5
 Expresso,2
 Tea,3
-'''
+"""
 
-beverages = pd.read_csv(io.StringIO(csv))
+beverages = pd.read_csv(io.StringIO(CSV))
 
-csv2 = """
+CSV2 = """
 food_item,food_price
 cookie juice,2.5
 chocolatine,2
 muffin,3
 """
 
-food_items = pd.read_csv(io.StringIO(csv))
+food_items = pd.read_csv(io.StringIO(CSV2))
 
-answer = """
+ANSWER_STR = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
-solution = db.query(answer)
+solution = db.query(ANSWER_STR)
 
-with st.sidebar :
+with st.sidebar:
     option = st.selectbox(
         "What would you like to review",
-        ['Join','Groupby','Windows Functions'],
-        index = None,
-        placeholder = "Select a theme ..."
+        ["Join", "Groupby", "Windows Functions"],
+        index=None,
+        placeholder="Select a theme ...",
     )
     st.write(f"You selected : {option}")
 
 st.header("enter your code")
-query = st.text_area(label = "votre code SQL ici",key = "user_input")
+query = st.text_area(label="Votre code SQL ici", key="user_input")
 if query:
     result = db.query(query)
     st.dataframe(result)
 
-tab2,tab3 = st.tabs(['Tables','Solution'])
+tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
     st.write("table : beverages")
@@ -55,7 +59,5 @@ with tab2:
     st.write("expected : ")
     st.dataframe(solution)
 
-with tab3 :
-    st.write(answer)
-
-
+with tab3:
+    st.write(ANSWER_STR)
