811127eeadcd4604b4a107e74e6a9938683f2087
piardito <piardito>
2025-09-30 11:01:01 +0200

    changement app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.write(
     Spaced Repetition System SQL practice"""
 )
 
-CSV = """
+CSV= """
 beverage,price
 orange juice,2.5
 Expresso,2
@@ -32,7 +32,7 @@ ANSWER_STR = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
-solution = db.query(ANSWER_STR)
+solution_df = db.query(ANSWER_STR).df()
 
 with st.sidebar:
     option = st.selectbox(
@@ -43,11 +43,27 @@ with st.sidebar:
     )
     st.write(f"You selected : {option}")
 
+
 st.header("enter your code")
 query = st.text_area(label="Votre code SQL ici", key="user_input")
 if query:
-    result = db.query(query)
+    result = db.query(query).df()
     st.dataframe(result)
+
+    if len(result.columns) != len(solution_df.columns):
+        st.write("Some colums are missing")
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("some columns are missing")
+
+    n_lines_differences = result.shape[0] - solution_df.shape[0]
+    if n_lines_differences != 0:
+        st.write(
+            f"result has a {n_lines_differences} lines differences with the solution"
+        )
+
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
@@ -57,7 +73,7 @@ with tab2:
     st.write("table : food_items")
     st.dataframe(food_items)
     st.write("expected : ")
-    st.dataframe(solution)
+    st.dataframe(solution_df)
 
 with tab3:
     st.write(ANSWER_STR)
