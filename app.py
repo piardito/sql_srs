1af1a8c9c5b805fa80093143780ca23d1190bce4
piardito <piardito>
2025-09-30 12:02:55 +0200

    ajout d'une database avec les exercices et un memory state

--- app.py
+++ app.py
@@ -10,70 +10,57 @@ st.write(
     Spaced Repetition System SQL practice"""
 )
 
-CSV= """
-beverage,price
-orange juice,2.5
-Expresso,2
-Tea,3
-"""
-
-beverages = pd.read_csv(io.StringIO(CSV))
-
-CSV2 = """
-food_item,food_price
-cookie juice,2.5
-chocolatine,2
-muffin,3
-"""
-
-food_items = pd.read_csv(io.StringIO(CSV2))
 
 ANSWER_STR = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
-solution_df = db.query(ANSWER_STR).df()
+
+con = db.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
+#solution_df = db.query(ANSWER_STR).df()
 
 with st.sidebar:
-    option = st.selectbox(
+    theme = st.selectbox(
         "What would you like to review",
-        ["Join", "Groupby", "Windows Functions"],
+        ("cross_joins", "Groupby", "Windows Functions"),
         index=None,
         placeholder="Select a theme ...",
     )
-    st.write(f"You selected : {option}")
+    st.write(f"You selected : {theme}")
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
+    st.write(exercise)
 
 
 st.header("enter your code")
 query = st.text_area(label="Votre code SQL ici", key="user_input")
-if query:
-    result = db.query(query).df()
-    st.dataframe(result)
+# if query:
+#     result = db.query(query).df()
+#     st.dataframe(result)
+#
+#     if len(result.columns) != len(solution_df.columns):
+#         st.write("Some colums are missing")
+#     try:
+#         result = result[solution_df.columns]
+#         st.dataframe(result.compare(solution_df))
+#     except KeyError as e:
+#         st.write("some columns are missing")
+#
+#     n_lines_differences = result.shape[0] - solution_df.shape[0]
+#     if n_lines_differences != 0:
+#         st.write(
+#             f"result has a {n_lines_differences} lines differences with the solution"
+#         )
+#
+#
+# tab2, tab3 = st.tabs(["Tables", "Solution"])
+#
+# with tab2:
+#     st.write("table : beverages")
+#     st.dataframe(beverages)
+#     st.write("table : food_items")
+#     st.dataframe(food_items)
+#     st.write("expected : ")
+#     st.dataframe(solution_df)
 
-    if len(result.columns) != len(solution_df.columns):
-        st.write("Some colums are missing")
-    try:
-        result = result[solution_df.columns]
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        st.write("some columns are missing")
-
-    n_lines_differences = result.shape[0] - solution_df.shape[0]
-    if n_lines_differences != 0:
-        st.write(
-            f"result has a {n_lines_differences} lines differences with the solution"
-        )
-
-
-tab2, tab3 = st.tabs(["Tables", "Solution"])
-
-with tab2:
-    st.write("table : beverages")
-    st.dataframe(beverages)
-    st.write("table : food_items")
-    st.dataframe(food_items)
-    st.write("expected : ")
-    st.dataframe(solution_df)
-
-with tab3:
-    st.write(ANSWER_STR)
+# with tab3:
+#     st.write(ANSWER_STR)
